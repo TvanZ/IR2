4fa3c764895b4f6f0870ed3ea2fe6cb1c0899741
masoumeh <mbakhtiariz@gmail.com>
2019-09-13 12:06:19 +0200

    for bella

--- process_data.py
+++ process_data.py
@@ -1,9 +1,44 @@
 import pretrainedmodels.dataset as dataset
 import pretrainedmodels.pretrained_models as prtr
 import numpy as np
+from optparse import OptionParser
 
 # TODO: what is this?....figure out
-args = [ some argparser stuff ]
+
+import argparse
+
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# parser.add_argument('dataset_info_path', metavar='N', type=str, nargs='+', default='pretrainedmodels.pretrained_models'
+#                     help='an integer for the accumulator')
+# parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                     const=sum, default=max,
+#                     help='sum the integers (default: find the max)')
+#
+# args = parser.parse_args()
+# print(args.accumulate(args.integers))
+#
+#
+
+parser = OptionParser()
+    # parameters to tune)
+parser.add_option('-d', '--dataset_info_path', dest='dataset_info_path'
+, help='frequency to perform validation')
+# TODO: before completing parameter tuning change back to False
+# TODO: otherwise, problems
+parser.add_option('-x', '--save_predictions', dest='save_predictions', default=True,
+                  help='export network predictions')
+(options, args) = parser.parse_args()
+
+
+
+
+
+
+
+
+
+
+# args = [ some argparser stuff ]
 
 # reading the Yahoo data
 data = dataset.get_dataset_from_json_info(
@@ -23,3 +58,7 @@ pretrain_model = prtr.read_model(args.model_file,  data, 1.0)
 scores = np.dot(data.feature_matrix, pretrain_model)
 
 # sort these scores in descending order, per query, data.doclist_ranges indicates the range of documents per query i.e. the indices it starts and ends, for query with id q: data.doclist_ranges[q:q+2]
+
+
+
+
