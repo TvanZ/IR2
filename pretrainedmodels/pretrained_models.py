dafb84b18bed5c17276b33a19724e910430e09a2
bellanicholson <nicholsonbella@gmail.com>
2019-09-13 09:57:44 +0200

    The pretrained models that Harri sent us.

---
+++ pretrainedmodels/pretrained_models.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def read_model(model_file_path, data, scale=1.0):
+  model = np.zeros(data.num_features)
+  with open(model_file_path, 'r') as model_file:
+    model_line = model_file.readlines()[-1]
+    model_line = model_line[:model_line.find('#')]
+    for feat_tuple in model_line.split()[1:]:
+      f_i, f_v = feat_tuple.split(':')
+      f_i = data.inverse_feature_map[int(f_i)]
+      model[f_i] = float(f_v)
+  model /= np.linalg.norm(model)/scale
+  return model
