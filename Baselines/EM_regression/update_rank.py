399d17ade9c0b3819afce617dcd0e1b08db9f347
Traian <ntvidrascu@gmail.com>
2019-09-21 16:45:10 +0200

    em regression

---
+++ Baselines/EM_regression/update_rank.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pickle as pk
+from collections import defaultdict
+
+DATABASE = "./database/"
+DEBUG = False
+
+
+def update_fold_rank(fold):
+    with open('qd_' + fold + '.pickle', 'rb') as handle:
+        qd = pk.load(handle)
+
+    file = open(DATABASE + fold + "/" + fold + ".trec.init_list")
+    for line in file:
+        line = line.split(' ')
+        qid = int(line[0])
+        idx = int(line[2].split('_')[-1])
+        rank = int(line[3])
+        qd[qid][idx]['rank'] = rank
+
+    if DEBUG:
+        print(qd[31074][32278]['rank'])
+        print(qd[31074][32278]['features'][16])
+
+    if not DEBUG:
+        with open('qd_' + fold + '.pickle', 'wb') as handle:
+            pk.dump(qd, handle, protocol=pk.HIGHEST_PROTOCOL)
